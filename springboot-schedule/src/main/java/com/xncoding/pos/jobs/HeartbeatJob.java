722cdb2e38effbfd9f2d04d5ba402c1ff6b03c57
Xiong Neng <xn@enzhico.com>
2018-09-15 15:33:45 +0800

    完成springboot-schedule升级重构

--- springboot-schedule/src/main/java/com/xncoding/pos/jobs/HeartbeatJob.java
+++ springboot-schedule/src/main/java/com/xncoding/pos/jobs/HeartbeatJob.java
@@ -27,7 +27,7 @@ public class HeartbeatJob {
     /**
      * 检查状态1
      */
-    @Scheduled(cron = "0 26 19 * * ?")
+    @Scheduled(cron = "0 30 12 * * ?")
     public void checkState1() {
         logger.info(">>>>> cron中午12:30上传检查开始....");
         logger.info(">>>>> cron中午12:30上传检查完成....");
@@ -36,7 +36,7 @@ public class HeartbeatJob {
     /**
      * 检查状态2
      */
-    @Scheduled(cron = "0 05 18 * * ?")
+    @Scheduled(cron = "0 0 18 * * ?")
     public void checkState2() {
         logger.info(">>>>> cron晚上18:00上传检查开始....");
         logger.info(">>>>> cron晚上18:00上传检查完成....");
